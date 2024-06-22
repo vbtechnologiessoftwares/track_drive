a79dd1281e6315fd047a13f34edb409fac0870cb
abhishek dhanwal <kft.abhishek@gmail.com>
2024-06-22 14:12:28 +0530

    route modification

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ExternalApiController;
 
 
-//Route::get('/', [ExternalApiController::class, 'index']);
+Route::get('/', [ExternalApiController::class, 'index']);
 
 Route::prefix('external-api')->group(function () {
     Route::get('/call-data', [ExternalApiController::class, 'index']);
