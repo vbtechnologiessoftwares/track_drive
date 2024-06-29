b9cf0274ea154f01bc15e785aa1e5cc33abee344
Vedant Bhatia <66814933+vbtechnologiessoftwares@users.noreply.github.com>
2024-06-29 14:51:17 +0530

    html code by vedant

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ExternalApiController;
+
+Route::get('/', [ExternalApiController::class, 'index2']);
+
+/*Route::get('/', [ExternalApiController::class, 'index']);
+*/
+Route::prefix('external-api')->group(function () {
+    Route::get('/call-data', [ExternalApiController::class, 'index']);
+    Route::get('/call-data1', [ExternalApiController::class, 'index1']);
+});
